fd791335b6e4ced25d5a3efe9a15fd225e791617
tarminik <tarminik@gmail.com>
2025-05-08 11:45:55 +0300

    push command implemented

--- cmd/dummy-admin/push.go
+++ cmd/dummy-admin/push.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"io/ioutil"
 
 	"github.com/spf13/cobra"
 )
 
 var pushCmd = &cobra.Command{
-	Use:   "push",
+	Use:   "push [config-file]",
 	Short: "Publish configuration to the server",
-	Long:  "... some long description ...",
+	Long:  "Read the configuration file and send it to the server (пока только выводит содержимое)",
+	Args:  cobra.ExactArgs(1), // Требуем ровно один аргумент: путь к файлу
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Configuration pushed to the server")
+		configPath := args[0]
+		// Читаем файл конфигурации
+		data, err := ioutil.ReadFile(configPath)
+		if err != nil {
+			fmt.Printf("Ошибка чтения файла %s: %v\n", configPath, err)
+			os.Exit(1)
+		}
+		// Выводим содержимое файла (заглушка)
+		fmt.Println("Содержимое конфигурационного файла:")
+		fmt.Println(string(data))
+		// TODO: Реализовать отправку на сервер
 	},
 }
 
